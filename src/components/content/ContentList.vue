0523721b25cc007579f792149d5e1bd523a9c57a
limuyi <jiaqi0724@live.com>
2018-03-26 18:38:43 +0800

    新增竞赛列表

---
+++ src/components/content/ContentList.vue
@@ -0,0 +1,133 @@
+<template>
+  <Panel>
+    <div slot="panel-header">
+      <h1>竞赛列表</h1>
+    </div>
+    <Card slot="panel-body">
+      <Table :columns="head" :data="content_data"></Table>
+    </Card>
+    <div slot="panel-page">
+      <Page :total="100" show-sizer show-total></Page>
+    </div>
+  </Panel>
+
+</template>
+
+<script>
+
+  import Panel from '../Panel'
+  import Card from "iview/src/components/card/card";
+
+  export default {
+    name: "content-list",
+    components: {
+      Card,
+      Panel
+    },
+    data() {
+      return {
+        head: [
+          {
+            title: '编号',
+            key: 'id'
+          },
+          {
+            title: '竞赛名称',
+            key: 'title',
+            render: (h, params) => {
+              return h('Button', {
+                props: {
+                  type: 'text',
+                  size: 'large'
+                },
+                on: {
+                  click: () => {
+                    this.$router.push({path: '/content/'+params.row.id})
+                  }
+                },
+                style: {
+                  padding: 0
+                }
+              }, params.row.title)
+            }
+          },
+          {
+            title: '状态',
+            key: 'state',
+            filters: [
+              {
+                label: '进行中',
+                value: 1
+              },
+              {
+                label: '已结束',
+                value: 2
+              }
+            ],
+            filterMultiple: false,
+            filterMethod (value, row) {
+              if (value === 1) {
+                return row.state === '进行中';
+              } else if (value === 2) {
+                return row.state === '已结束';
+              }
+            }
+          },
+          {
+            title: '截止时间',
+            key: 'dead_time'
+          },
+          {
+            title: '类型',
+            key: 'type',
+            filters: [
+              {
+                label: '公有',
+                value: 1
+              },
+              {
+                label: '私有',
+                value: 2
+              }
+            ],
+            filterMultiple: false,
+            filterMethod (value, row) {
+              if (value === 1) {
+                return row.type === '公有';
+              } else if (value === 2) {
+                return row.type === '私有';
+              }
+            }
+          }
+        ],
+        content_data: [
+          {
+            id: '1001',
+            title: '测试竞赛',
+            state: '进行中',
+            dead_time: '2018-4-1',
+            type: '公有'
+          },
+          {
+            id: '1002',
+            title: '测试竞赛名字很长名字很长很长很长很长应该不会长到这个程度了或者还能再长一点',
+            state: '已结束',
+            dead_time: '2018-1-1',
+            type: '私有'
+          },
+          {
+            id: '1003',
+            title: '测试竞赛',
+            state: '进行中',
+            dead_time: '2018-4-1',
+            type: '公有'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,10 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import ProblemList from '@/components/problem/ProblemList'
 import Problem from '@/components/problem/Problem'
+import ContentList from '@/components/content/ContentList'
+import Content from '@/components/content/Content'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -22,6 +24,16 @@ export default new Router({
       path: '/problem/:id',
       name: 'Problem',
       component: Problem
+    },
+    {
+      path: '/content-list',
+      name: 'ContentList',
+      component: ContentList
+    },
+    {
+      path: '/content/:id',
+      name: 'Content',
+      component: Content
     }
   ]
 })
