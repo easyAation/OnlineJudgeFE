f9f0475540f74ff5e842e5fa4b5d5d7c725c5a6d
limuyi <jiaqi0724@live.com>
2018-04-06 22:25:44 +0800

    新增axios

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview'
+import axios from 'axios'
 import 'iview/dist/styles/iview.css'
 
 
 Vue.config.productionTip = false
 
 Vue.use(iView);
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
