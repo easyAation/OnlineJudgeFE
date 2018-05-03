2ead4850c22c43ecf5f92ce7c952f02d9f53cae8
limuyi <jiaqi0724@live.com>
2018-05-03 09:33:25 +0800

    添加部分数据获取接口

--- src/api.js
+++ src/api.js
@@ -7,13 +7,34 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export default {
-  getProblemCount() {
-    return axios('/problem_count', {
+  getProblemNum() {
+    return axios('problems/problem_num', {
       method: 'get',
     })
   },
+  getContestNum() {
+    return axios('/contests/contest_num', {
+      method: 'get',
+    })
+  },
+  getStatusNum() {
+    return axios('/status/status_num', {
+      method: 'get',
+    })
+  },
+  getAcNum() {
+    return axios('/rank/ac_num', {
+      method: 'get',
+    })
+  },
+  getAchievementNum() {
+    return axios('/rank/achievement_num', {
+      method: 'get',
+    })
+  },
+  //获取题目列表
   getProblemList (offset, limit, searchParams) {
-    console.log("get page:" + offset +"  " + limit);
+    console.log("get problem page:" + offset + "  " + limit);
     let params = {
       offset: offset,
       limit: limit
@@ -29,5 +50,102 @@ export default {
       method: 'get',
       params: params
     })
-  }
+  },
+  //返回题目信息
+  getProblem (problemID) {
+    let params = {
+      problemID: problemID
+    };
+    return axios('/problems/problem', {
+      method: 'get',
+      params: params
+    })
+  },
+  //获取竞赛列表
+  getContestList (offset, limit) {
+    console.log("get contest page:" + offset + " " + limit);
+    let params = {
+      offset: offset,
+      limit: limit
+    };
+    return axios('/contests/contest_list', {
+      method: 'get',
+      params: params
+    })
+  },
+  //获取竞赛信息
+  getContestInfo (contestID) {
+    let params = {
+      contestID: contestID
+    };
+    return axios('/contests/contest_info', {
+      method: 'get',
+      params: params
+    })
+  },
+  //获取竞赛题目
+  getContestProblem (contestID) {
+    let params = {
+      contestID: contestID
+    };
+    return axios('/contests/problem', {
+      method: 'get',
+      params: params
+    })
+  },
+  //获取竞赛排名
+  getContestRank (contestID) {
+    let params = {
+      contestID: contestID
+    };
+    return axios('/contests/problem', {
+      method: 'get',
+      params: params
+    })
+  },
+  getContestRankProblem (contestID) {
+    let params = {
+      contestID: contestID
+    };
+    return axios('/contests/rank_problems', {
+      method: 'get',
+      params: params
+    })
+  },
+  //获取状态列表
+  getStatus (offset, limit) {
+    console.log("get status page:" + offset + " " + limit);
+    let params = {
+      offset: offset,
+      limit: limit
+    };
+    return axios('/status', {
+      method: 'get',
+      params: params
+    })
+  },
+  //获取解题排名列表
+  getAcRank (offset, limit) {
+    console.log("get status page:" + offset + " " + limit);
+    let params = {
+      offset: offset,
+      limit: limit
+    };
+    return axios('/rank/ac', {
+      method: 'get',
+      params: params
+    })
+  },
+  //获取成绩排名列表
+  getAchievementRank (offset, limit) {
+    console.log("get status page:" + offset + " " + limit);
+    let params = {
+      offset: offset,
+      limit: limit
+    };
+    return axios('/rank/achievement', {
+      method: 'get',
+      params: params
+    })
+  },
 }
